a1c9cef4f34186bd9cde4c9422bd73ee4977a7ad
ColinThePanda <urboycolinthepanda@gmail.com>
2025-10-19 19:48:22 -0400

    fix build.bat

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ setlocal
 echo Cleaning old builds...
 rmdir /S /Q build 2>nul
 rmdir /S /Q dist 2>nul
-rmdir /S /Q src/panda_terminal.egg-info
+rmdir /S /Q src\panda_terminal.egg-info
 
 echo Building package...
 python -m build
